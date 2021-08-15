b2d5c4d03309f464ca7b0218762e4792dc096178
engrbugs <engrbugs@gmail.com>
2021-08-15 12:58:30 -0700

    2021-08-15-1258

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from bq_helper import BigQueryHelper
+# install this https://github.com/SohierDane/BigQuery_Helper
+
 import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=".kaggle/endless-beach-228506-b15de187c224.json"
+# Google Credentials Path json file.
+# https://youtu.be/iolQX4XJN2A?t=65
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = ".kaggle/endless-beach-228506-b15de187c224.json"
 
 bq_assistant = BigQueryHelper(active_project="bigquery-public-data",
                                    dataset_name="chicago_crime")
@@ -9,6 +13,5 @@ print(bq_assistant.table_schema("crime"))
 dataset = bq_assistant.dataset
 tables = list(bq_assistant.list_tables())
 
-# Print number of tables in the dataset
 print(len(tables))
-num_tables = len(tables)  # Store the answer as num_tables and then run this cell
+num_tables = len(tables)
